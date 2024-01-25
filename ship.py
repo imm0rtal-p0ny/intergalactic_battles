cef1f568c4664dc0f8b11eceae2b6f96c167b3df
Maksym Vasyltsiv <kasta027@gmail.com>
2024-01-25 20:34:26 +0200

    first save

---
+++ ship.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+        self.image = pygame.image.load('images/alien_ship.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.midleft = self.screen_rect.midleft
+        self.moving_down = False
+        self.moving_up = False
+        self.y = float(self.rect.y)
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        self.rect.y = self.y
